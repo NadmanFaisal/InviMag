e606f13daf6b0c33348c608a2ee5f78ac58f2a04
Mohamed Taha Jasser <mohamedt@student.chalmers.se>
2024-09-17 22:50:47 +0200

    #30 Add both GET Methods for Suppliers and Suppliers id

--- server/controllers/SupplierController.js
+++ server/controllers/SupplierController.js
@@ -19,4 +19,30 @@ router.post('/suppliers', async function (req, res, next) {
     }
 });
 
+// Gets all suppliers from the database
+
+router.get('/suppliers', async function(req, res) {
+    try {
+        const suppliers = await Supplier.find();
+        res.json({'suppliers': suppliers});
+    } catch (error) {
+        res.status(500).json({ error: 'Server error when getting all suppliers' }); // 500: Server Errors
+    }
+});
+
+// Get a specific supplier by their ids
+
+router.get('/suppliers/:id', async function (req, res) {
+    var id = req.params.id;
+    try {
+        const supplier = await Supplier.findById(id);
+        if (!supplier) {
+            return res.status(404).json({"message": "Did not find supplier"}); // 404: Not found 
+        }
+        res.json(supplier);
+    } catch (error) {
+        res.status(500).json({ error: 'Server error when getting supplier from id' });
+    }
+});
+
 module.exports = router;
