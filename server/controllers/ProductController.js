2cef915e3977e41c25ed70a6171a4af1a07c243e
Vaibhav Puram <guspurva@student.gu.se>
2024-09-23 14:01:43 +0200

    #35 Refactor controller and app class to initialise variables with const instead of var

--- server/controllers/ProductController.js
+++ server/controllers/ProductController.js
@@ -1,4 +1,5 @@
 
+const { listen } = require('../app');
 const Product = require('../models/Product')
 
 exports.createProduct = async (req, res, next ) => {
@@ -35,7 +36,7 @@ exports.getAllProducts =  async (req, res) =>  {
         const products = await Product.find().sort(sort_type);
         res.json({"Products" : products});
     } catch (error){
-        res.status(500).json({ error: 'An error occurred while fetching Products yo' });
+        res.status(500).json({ error: 'An error occurred while fetching Products' });
     }
 
 }
@@ -53,32 +54,30 @@ exports.getProductByName = async (req, res) => {
     }
     res.status(200).json({"Products": productNames});
     } catch (error){
-        res.status(500).json({ error: 'An error occurred while fetching Products hi' });
+        res.status(500).json({ error: 'An error occurred while fetching Products' });
     }
 
 }
 
 exports.getProductByID = async (req, res) => {
-    var productID = req.params.id;
+    const productID = req.params.id;
     try{
-    var products = await Product.findById(productID);
+    const products = await Product.findById(productID);
     if(!products){
         res.status(404).json({"message" : "did not find product"})
     }
     res.status(200).json(products);
     } catch (error){
-        res.status(500).json({ error: 'An error occurred while fetching Products siu' });
+        res.status(500).json({ error: 'An error occurred while fetching Products' });
     }
 
 }
 
 
 
-
-
 exports.deleteAllProducts = async (req, res) => {
     try {
-        var product = await Product.deleteMany();
+        const product = await Product.deleteMany();
 
         if (!product) {
             return res.status(404).json({ message: 'Product not found' });
@@ -93,14 +92,14 @@ exports.deleteAllProducts = async (req, res) => {
 
 exports.deleteProductByID = async (req, res) => {
     try {
-        var productID = req.params.id;
-        var product = await Product.findByIdAndDelete(productID);
+        const productID = req.params.id;
+        const product = await Product.findByIdAndDelete(productID);
 
         if (!product) {
             return res.status(404).json({ message: 'Product not found' });
         }
 
-        res.status(200).json({message : 'Product Deleted successfully'}); //Return deleted product, 200: Return OK response
+        res.status(200).json({message : 'Product deleted successfully'}); //Return deleted product, 200: Return OK response
     } catch (error) {
         res.status(500).json({ message: 'Server error', error: error.message });
     }
@@ -109,7 +108,7 @@ exports.deleteProductByID = async (req, res) => {
 
 exports.updateProductByIB = async (req, res) => {
     try {
-        var id = req.params.id;
+        const id = req.params.id;
         const {name, quantity, buying_price, selling_price, category, in_stock} = req.body;
 
         // this to ensure that put does not act like patch.
@@ -128,10 +127,10 @@ exports.updateProductByIB = async (req, res) => {
             return res.status(404).json({ message: 'in_stock cannot be empty' });
         }
 
-        var updatedProduct = {name, quantity, buying_price, selling_price, category, in_stock};
+        const updatedProduct = {name, quantity, buying_price, selling_price, category, in_stock};
 
         // true: to always return updated product
-        var product = await Product.findByIdAndUpdate(id, updatedProduct, { new: true });
+        const product = await Product.findByIdAndUpdate(id, updatedProduct, { new: true });
         if (!product) {
             return res.status(404).json({ message: 'Product was not found' });
         }
@@ -147,13 +146,13 @@ exports.updateProductByIB = async (req, res) => {
 
 exports.partialUpdateProduct = async (req, res) => {
     try{
-        var productID = req.params.id;
-        var products = await Product.findById(productID);
+        const productID = req.params.id;
+        const products = await Product.findById(productID);
         if(!products){ // if the product with the inputted ID does not exist, then throw a 404 not found error
             res.status(404).json({"message" : "did not find product"})
         }
         // Creating a reference to an updated product
-        var updated_product ={
+        const updated_product ={
             name: req.body.name || products.name,
             quantity: req.body.quantity || products.quantity,
             buying_price: req.body.buying_price || products.buying_price,
@@ -163,7 +162,7 @@ exports.partialUpdateProduct = async (req, res) => {
             business_owner : req.body.businessOwner || products.businessOwner
         };
         
-        var new_products = await Product.findByIdAndUpdate(productID, updated_product, {new: true}); // assigning the old product reference to the new updated product
+        const new_products = await Product.findByIdAndUpdate(productID, updated_product, {new: true}); // assigning the old product reference to the new updated product
         res.json(new_products);
     }catch(error){
         res.status(500).json({ error: 'An error occurred while fetching Products' });
