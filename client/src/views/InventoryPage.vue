de424c6a32c7c57d0470c1da01907112cd5058b0
Vaibhav Puram <guspurva@student.gu.se>
2024-10-03 12:13:39 +0200

    #46 Implement front end logic to display all products using the backend GET HTTP method

--- client/src/views/InventoryPage.vue
+++ client/src/views/InventoryPage.vue
@@ -1,24 +1,48 @@
 <template>
     <div>
         <h1> Inventory Page</h1>
-
+        <div v-if="products.length">
+      <h2>Products:</h2>
+      <ul>
+        <li v-for="product in products" :key="product._id">
+          <p>Name: {{ product.name }}</p>
+          <p>Quantity: {{ product.quantity }}</p>
+          <p>Buying Price: {{ product.buying_price }}</p>
+          <p>Selling Price: {{ product.selling_price }}</p>
+          <p>Category: {{ product.category }}</p>
+          <p>In Stock: {{ product.in_stock }}</p>
+        </li>
+      </ul>
+    </div>
 
     </div>
     
 </template>
 
 <script>
-import { Api } from '@/Api'
+import { productApi} from '@/api/ProductApi'
 export default {
     name: "inventoryPage",
     data(){
         return {
-            const product
+            products: []
+        }
+    },
 
-
+    created(){
+        this.fetchProducts
+    },
+    
+    methods:{
+        async fetchProducts(){
+            try{
+                const response = await productApi.getAllProducts();
+                this.products = response;
+            }catch(error){
+                console.error('Error fetching products:', error);
+            }
         }
     }
-
 
 }
 
