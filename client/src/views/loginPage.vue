267321bb8ffc81efdd00f506edbb06a3081a5068
Nadman Faisal <nadman@chalmers.se>
2024-10-03 11:43:59 +0200

    Create non functional login container

--- client/src/views/loginPage.vue
+++ client/src/views/loginPage.vue
@@ -29,7 +29,12 @@
             />
           </div>
 
-          <button type="submit">Login</button>
+          <div class="form-group">
+            <input type="checkbox" id="rememberMe" v-model="rememberMe" class="checkbox" />
+            <label for="rememberMe" class="remember-label">Remember me</label>
+          </div>
+
+          <button type="submit">Log In</button>
         </form>
         <div v-if="error" class="error-message">
           {{ error }}
@@ -69,32 +74,27 @@ export default {
 <style scoped>
 .split-screen {
   display: flex;
-  height: 100vh; /* Make sure the height covers the full viewport */
+  height: 100vh;
 }
 
-/* Left section (blue) */
 .left-section {
-  flex: 1; /* Takes up 50% of the screen */
-  background-color: #FFFFFF; /* Blue background */
+  flex: 3;
+  background-color: #FFFFFF;
 }
 
 .right-section {
-  flex: 1; /* Takes up the other 50% of the screen */
-  background-color: #3377FF; /* White background */
+  flex: 2;
+  background-color: #3377FF;
   display: flex;
   justify-content: center;
-  align-items: center; /* Center the login form */
+  align-items: center;
 }
 
 h2 {
   display: flex;
-  width: 236px;
-  height: 61px;
-  flex-direction: column;
   justify-content: center;
-  flex-shrink: 0;
+  align-items: center;
 
-  /* Typography */
   color: #000;
   text-align: center;
   font-family: "Istok Web", sans-serif;
@@ -106,15 +106,15 @@ h2 {
 
 .login-container {
   max-width: 400px;
-  height: 700px;
-  padding: 20px;
+  height: 500px;
+  padding: 40px;
   border: 1px solid #ccc;
   border-radius: 50px;
   background: #FFF;
   box-shadow: -15px 15px 4px 0px rgba(0, 0, 0, 0.25);
 }
 
-label[for="email"] {
+label[for="email"], label[for="password"] {
   display: flex;
   width: 163px;
   height: 42px;
@@ -123,33 +123,17 @@ label[for="email"] {
   flex-shrink: 0;
 
   color: #000;
-  text-align: center;
+  text-align: left;
   font-family: "Istok Web", sans-serif;
   font-size: 20px;
   font-style: normal;
   font-weight: 700;
   line-height: normal;
-}
-
-label[for="password"] {
-  display: flex;
-  width: 163px;
-  height: 42px;
-  flex-direction: column;
-  justify-content: center;
-  flex-shrink: 0;
-
-  color: #000;
-  text-align: center;
-  font-family: "Istok Web", sans-serif;
-  font-size: 20px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
+  padding-left: 10px;
 }
 
 .form-group {
-  margin-bottom: 15px;
+  margin-bottom: 25px;
 }
 
 input {
@@ -164,6 +148,24 @@ input {
   outline: none;
 }
 
+.checkbox {
+  width: 14px;
+  height: 13px;
+  flex-shrink: 0;
+  margin-right: 5px;
+  margin-left: -200px;
+}
+
+label[for="rememberMe"] {
+  color: #939393;
+  text-align: center;
+  font-family: "Istok Web";
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+
 button {
   width: 278px;
   height: 59px;
@@ -171,10 +173,17 @@ button {
   border: none;
   border-radius: 50px;
   background: #37F;
+  text-align: center;
+  font-family: "Istok Web";
+  color: #FFF;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
 }
 
 button:hover {
-  background-color: rgb(109, 157, 255);
+  background-color: rgb(0, 85, 255);
 }
 
 .error-message {
