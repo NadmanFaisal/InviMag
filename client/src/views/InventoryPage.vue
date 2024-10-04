9e34ff922c4330a1263bbb99d7f40eefcd000ca8
Vaibhav Puram <guspurva@student.gu.se>
2024-10-04 11:21:49 +0200

    #46 Implement HTML logic to display products in the inventory page

--- client/src/views/InventoryPage.vue
+++ client/src/views/InventoryPage.vue
@@ -1,24 +1,31 @@
 <template>
   <div>
     <h1>Inventory Page</h1>
-    <div>
+    <div v-if="products.length">
       <h2>Products:</h2>
-      
+      <ul>
+        <li v-for="product in products" :key="product._id" class="product-item">
+          <h3>{{ product.name }}</h3>
+          <p><strong>Quantity:</strong> {{ product.quantity }}</p>
+          <p><strong>Buying Price:</strong> ${{ product.buying_price }}</p>
+          <p><strong>Selling Price:</strong> ${{ product.selling_price }}</p>
+          <p><strong>Category:</strong> {{ product.category }}</p>
+          <p><strong>In Stock:</strong> {{ product.in_stock ? 'Yes' : 'No' }}</p>
+        </li>
+      </ul>
     </div>
-    <div> {{Products}}</div>
-    <div>
+    <div v-else>
       <p>No products available.</p>
     </div>
   </div>
 </template>
-
 <script>
 import { productApi } from '@/api/ProductApi'
 export default {
     name: "inventoryPage",
     data(){
         return {
-            Products: []
+            products: [],
         }
     },
 
@@ -30,8 +37,8 @@ export default {
         async fetchProducts(){
             try{
                 const response = await productApi.getAllProducts();
-                this.Products = response.data;
-                console.log(this.Products);
+                this.products = response.data.Products;
+                console.log(this.products);
 
             }catch(error){
                 console.error('Error fetching products:', error);
