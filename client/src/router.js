f641a8963ffa60b8b8240ead3919ff670a852cf6
Mohamed Taha Jasser <mohamedt@student.chalmers.se>
2024-10-05 15:10:50 +0200

    #44 Changed OrderHistory to OrderHistory.js

--- client/src/router.js
+++ client/src/router.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from './views/Home.vue'
+import Create from './views/SignUp.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: Home }
+  { path: '/', name: 'home', component: Home },
+  { path: '/signup', name: 'create', component: Create }
 ]
 
 const router = createRouter({
