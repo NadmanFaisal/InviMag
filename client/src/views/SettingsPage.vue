aef9eb8d81d337ff203951f267b35bd98599356c
Vaibhav Puram <guspurva@student.gu.se>
2024-10-12 13:12:33 +0200

    #49 Integrate new sidebar into settings page

--- client/src/views/SettingsPage.vue
+++ client/src/views/SettingsPage.vue
@@ -4,27 +4,24 @@
       <b-row class="min-vh-100">
 
         <!-- Navigation bar on the left -->
-        <NavigationBar/>
-
+        <Sidebar/>
+    <b-col cols = "10" class = "offset-2 content-section" >    
+      <b-row>
         <b-col cols="5" class="middle-section">
-
           <!-- Profile Overview -->
           <ProfileOverview />
-
           <!-- Account Settings -->
           <AccountSettings />
-
         </b-col>
 
         <b-col cols="5" class="right-section">
-
           <!-- Password Settings -->
           <PasswordSettings />
-
           <!-- Budget Settings -->
           <BudgetSettings/>
-
         </b-col>
+      </b-row>
+    </b-col>
 
       </b-row>
     </b-container>
@@ -38,6 +35,7 @@ import AccountSettings from '../components/SettingsForms/AccountSettingsForm.vue
 import PasswordSettings from '../components/SettingsForms/PasswordSettingsForm.vue'
 import BudgetSettings from '../components/SettingsForms/BudgetSettingsForm.vue'
 import NavigationBar from '../components/SettingsForms/NavigationForm.vue'
+import Sidebar from '../components/InventoryComponents/Sidebar.vue'
 
 export default {
   components: {
@@ -45,7 +43,8 @@ export default {
     AccountSettings,
     PasswordSettings,
     BudgetSettings,
-    NavigationBar
+    NavigationBar,
+    Sidebar
   },
   data() {
     return {
@@ -67,9 +66,24 @@ export default {
   padding: 0;
 }
 
+
 .middle-section, .right-section {
   display: flex;
   flex-direction: column;
+  padding: 20px;
+  background: #f1f1f1;
+}
+
+.sidebar{
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 16.666667%;
+  background-color: #37F;
+}
+
+.content-section {
   padding: 20px;
   background: #f1f1f1;
 }
@@ -84,11 +98,17 @@ export default {
     width: 100%;
     padding: 10px;
   }
+  .content-section {
+    flex: none;
+    width: 100%;
+    padding: 10px;
+  }
 
    .left-section {
     display: none;
    }
 
 }
+
 
 </style>
