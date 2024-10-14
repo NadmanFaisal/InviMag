d4b1ee7e2a45ee0843cfee0c693bfa9c5dc63cde
Nadman Faisal <nadman@chalmers.se>
2024-10-14 21:14:28 +0200

    #52 Create Order history forms for easier refactoring

---
+++ client/src/components/OrdersComponents/OrderHistoryForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-col cols="12" class="order-history-container">
+
+  </b-col>
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+  name: 'OrderHistoryContainer',
+  data() {
+    return {
+      userId: '',
+      orders: []
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
