24ca1d5219b0469779bace860ce5dd9339597b35
Vaibhav Puram <guspurva@student.gu.se>
2024-10-15 14:09:46 +0200

    #51 Create a new page for all the suppliers

--- client/src/views/SuppliersPage.vue
+++ client/src/views/SuppliersPage.vue
@@ -1,18 +1,32 @@
 <template>
-    <div>
+    <b-col>
+      <b-col :cols = "2"></b-col>
       <b-container fluid>
-        <h1 class="display-5 fw-bold">Suppliers</h1>
+        <h1 class="display-5 fw-bold supplierCardStyle">Suppliers</h1>
+        <b-col :cols="10" class = "supplierCardStyle">
+          <h1 class="input-hint-style">Click the supplier to view their products.</h1>
+        <ul>
+          <SupplierCard v-for="supplier in suppliers" :key="supplier._id" :supplier = "supplier"></SupplierCard>  
+        </ul>
+        </b-col>
         <p class="fs-4"></p>
       </b-container>
-    </div>
+    </b-col>
   </template>
   
   <script>
   import {supplierApi} from '../api/SupplierApi';
+  import SupplierCard from '../components/SupplierComponents/SupplierCard.vue'
 export default {
+    components:{
+      SupplierCard
+    },
     name: "SuppliersPage",
     data(){
+      return{
       suppliers: []
+      
+      }
     },
 
     created(){
@@ -41,3 +55,26 @@ export default {
 
 
   </script>
+
+  <style scoped>
+  .input-hint-style{
+    font-family: "Istok Web";
+    font-size: 15px;
+    color: gray;
+    text-align: center;
+  }
+.testDisplayFont{
+  color: black;
+  font-size: 20px;
+  font-weight: bold;
+  font-family: "Istok Web", "Sans Serif";
+}
+
+.supplierCardStyle{
+  margin-left: 16.66667%;
+}
+
+
+
+
+  </style>
