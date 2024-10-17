c579aeec06131034ef3f4420bd9ed275fd1d27f1
Mohamed Taha Jasser <mohamedt@student.chalmers.se>
2024-10-17 20:54:12 +0200

    #53 Fix title styling for costly

--- client/src/components/HomeComponents/CostlyProducts.vue
+++ client/src/components/HomeComponents/CostlyProducts.vue
@@ -1,6 +1,9 @@
 <template>
     <b-card class="mb-3 costly-products-card">
-      <h5 class="card-title">COSTLY PRODUCTS</h5>
+        <div class="title-container">
+        <b class="title">COSTLY PRODUCTS</b>
+      </div>
+      <hr />
       <div class="product-item">
         <p>Blue polo tshirt<br><strong>35 Pcs</strong></p>
       </div>
@@ -24,5 +27,25 @@ export default {
     width: 50px;
     height: 50px;
     margin-bottom: 5px;
+  }
+  hr {
+    margin-top: 0;
+    margin-bottom: 1rem;
+    height: 2px;
+    background: #494949;
+    width: 100%;
+  }
+
+  .title-container {
+    width: 100%;
+  }
+
+  .title {
+    color: #787676;
+    font-family: "Istok Web";
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 100%;
   }
   </style>
