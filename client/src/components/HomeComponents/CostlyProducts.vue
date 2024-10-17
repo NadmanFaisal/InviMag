749edc9643930a04d2016e495dbb9d6fe4e2a6b5
Mohamed Taha Jasser <mohamedt@student.chalmers.se>
2024-10-17 14:54:57 +0200

    #53 Add basic Costly Products Container for Home Screen

---
+++ client/src/components/HomeComponents/CostlyProducts.vue
@@ -0,0 +1,28 @@
+<template>
+    <b-card class="mb-3 costly-products-card">
+      <h5 class="card-title">COSTLY PRODUCTS</h5>
+      <div class="product-item">
+        <p>Blue polo tshirt<br><strong>35 Pcs</strong></p>
+      </div>
+    </b-card>
+  </template>
+
+<script>
+export default {
+  name: 'CostlyProducts'
+}
+</script>
+
+  <style scoped>
+  .costly-products-card {
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+  }
+
+  .product-image {
+    width: 50px;
+    height: 50px;
+    margin-bottom: 5px;
+  }
+  </style>
