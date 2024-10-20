34a21968ed27ac1cbf501a4d9d0006eab9990249
Mohamed Taha Jasser <mohamedt@student.chalmers.se>
2024-10-20 13:23:45 +0200

    #53 Add in stock, out of stock and almost sold out backend for the overview tab

--- client/src/components/HomeComponents/Overview.vue
+++ client/src/components/HomeComponents/Overview.vue
@@ -5,18 +5,18 @@
         <b id="overview">Overview</b>
         <b-row class="justify-content-center g-3">
           <b-col cols="auto" class="group-child me-4">
-            <b class="b">98</b>
-            <b class="to-be-shipped">TO BE SHIPPED</b>
+            <b class="b">{{ inStockCount }}</b>
+            <b class="to-be-shipped">IN STOCK</b>
           </b-col>
 
           <b-col cols="auto" class="group-child me-4">
-            <b class="b" style="color: green;">98</b>
-            <b class="to-be-shipped" style="color: green;">DELIVERED</b>
+            <b class="b" style="color: #C05236;">{{ almostSoldOutCount }}</b>
+            <b class="to-be-shipped" style="color: #C05236;">ALMOST SOLD OUT</b>
           </b-col>
 
           <b-col cols="auto" class="group-child">
-            <b class="b" style="color: red;">98</b>
-            <b class="to-be-shipped" style="color: red;">RETURNED</b>
+            <b class="b" style="color: #e94040;">{{ outOfStockCount }}</b>
+            <b class="to-be-shipped" style="color: #e94040;">OUT OF STOCK</b>
           </b-col>
         </b-row>
       </div>
@@ -42,7 +42,10 @@ export default {
   data() {
     return {
       userId: '',
-      total_budget: 0
+      total_budget: 0,
+      inStockCount: 0,
+      almostSoldOutCount: 0,
+      outOfStockCount: 0
     }
   },
   computed: {
@@ -55,6 +58,7 @@ export default {
     if (businessOwner && businessOwner.id) {
       this.userId = businessOwner.id
       this.displayUserTotalBudget()
+      this.getTotalInventoryProducts()
     }
   },
   methods: {
@@ -65,6 +69,31 @@ export default {
       } catch (error) {
         console.error('Error getting user total budget:', error)
         alert('Could not get your total budget. Please try again.')
+      }
+    },
+    async getTotalInventoryProducts() {
+      try {
+        const response = await Api.get(`http://localhost:3000/v1/api/BusinessOwners/${this.userId}`)
+        const productIds = response.data.products
+
+        this.inStockCount = 0
+        this.almostSoldOutCount = 0
+        this.outOfStockCount = 0
+
+        for (const productId of productIds) {
+          const fetchedProduct = await Api.get(`http://localhost:3000/v1/api/Products/${productId}`)
+          const productQuantity = Number(fetchedProduct.data.quantity)
+
+          if (productQuantity > 3) {
+            this.inStockCount++
+          } else if (productQuantity > 0 && productQuantity <= 3) {
+            this.almostSoldOutCount++
+          } else if (productQuantity === 0) {
+            this.outOfStockCount++
+          }
+        }
+      } catch (error) {
+        console.error('Error fetching products:', error)
       }
     }
   }
@@ -129,13 +158,13 @@ export default {
 
 .b {
   font-size: 36px;
-  color: #ff8833;
+  color: green;
   line-height: 1;
 }
 
 .to-be-shipped {
   font-size: 14px;
-  color: #ff8833;
+  color: green;
 }
 
 @media (max-width: 768px) {
