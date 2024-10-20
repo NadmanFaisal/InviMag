a773feea7e02d6890ba2f34a180e5c55f3cc8001
Mohamed Taha Jasser <mohamedt@student.chalmers.se>
2024-10-20 14:44:08 +0200

    #53 Add comments on Supplier API

--- client/src/api/SupplierApi.js
+++ client/src/api/SupplierApi.js
@@ -1,22 +1,26 @@
+/**
+ * Add Product API methods for Product APIs
+ */
 import { Api } from '../Api'
 
 export const supplierApi = {
+  // create supplier
   createProduct(newSupplier) {
     return Api.post('/suppliers', newSupplier)
   },
-
+  // to get all suppliers
   getAllSuppliers() {
     return Api.get('/suppliers/')
   },
-
+  // to get all suppliers by sorting with name
   getAllSuppliersByName(supplierName) {
     return Api.get(`/suppliers/search/?name=${supplierName}`)
   },
-
+  // to get supplier by id
   getSupplierByID(supplierId) {
     return Api.get(`/suppliers/${supplierId}`)
   },
-
+  // to get all suppliers
   getSupplierProducts(supplierId) {
     return Api.get(`/suppliers/${supplierId}/Products`)
   }
