37bf062814e5f3ef601b8b0eabb54ca27962eb53
Mohamed Taha Jasser <mohamedt@student.chalmers.se>
2024-10-21 19:45:05 +0200

    #61 Add comments on Amount Spent on Orders

--- client/src/api/BusinessOwnerApi.js
+++ client/src/api/BusinessOwnerApi.js
@@ -1,16 +1,18 @@
-import { Api } from '../Api';
+import { Api } from '../Api'
 
-  export const businessOwnerApi = {
-    
-    getBusinessOwnerProducts(id){
-        return Api.get(`/BusinessOwners/${id}/products`);
-    },
+export const businessOwnerApi = {
 
-    getAllProductsByBuyingPrice(id){
-        return Api.get(`/BusinessOwners/${id}/products/orderByBuyingPrice`);
-    },
-    getAllProductsByQuantity(id){
-        return Api.get(`/BusinessOwners/${id}/products/orderByQuantity`);
-    },
+  // gets products from business owners id
+  getBusinessOwnerProducts(id) {
+    return Api.get(`/BusinessOwners/${id}/products`)
+  },
+  // gets all products sorted by buying price with business owner id
+  getAllProductsByBuyingPrice(id) {
+    return Api.get(`/BusinessOwners/${id}/products/orderByBuyingPrice`)
+  },
+  // gets all products sorted by quantity with business owner id
+  getAllProductsByQuantity(id) {
+    return Api.get(`/BusinessOwners/${id}/products/orderByQuantity`)
+  }
 
 }
