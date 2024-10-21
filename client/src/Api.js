7d04a91dcebf2c0f04cbf6105ed07746a4a45f98
Mohamed Taha Jasser <mohamedt@student.chalmers.se>
2024-10-21 20:48:02 +0200

    #61 Add clarification for Api.js

--- client/src/Api.js
+++ client/src/Api.js
@@ -1,12 +1,15 @@
 import axios from 'axios'
 
-axios.defaults.withCredentials = true
+axios.defaults.withCredentials = true // defauts axios into pushing cookies
 
 export const Api = axios.create({
   baseURL: import.meta.env.VITE_API_ENDPOINT || 'http://localhost:3000/v1/api',
-  withCredentials: true
+  withCredentials: true // for the axios to push cookies, incase the defaulter didnt work
 })
 
+// acts as an intercepter to check what the backend has sent,
+// if backend sent an error meaning user not authorised or a problem has occured and sends
+// user back to login screen. (ex: if user cookies expire we force them back to login)
 axios.interceptors.response.use(
   response => {
     return response
